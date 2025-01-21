4fb9fec2e2d1c46bd9bfcfff85bc57a10752bc0f
ArtisticGAMERR <151627696+ArtisticGAMERR@users.noreply.github.com>
2025-01-21 12:47:22 +0100

    Add files via upload
    code 1

---
+++ Code1/app/src/main/java/com/example/createacc/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.createacc
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textview.MaterialTextView
+
+class MainActivity : AppCompatActivity() {
+    var btnLogin:MaterialTextView?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        btnLogin = findViewById(R.id.loginText)
+        btnLogin?.setOnClickListener{
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
+         }
+}
